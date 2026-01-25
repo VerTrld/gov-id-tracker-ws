63e441956c3e5ab4aa08d27b3355c01040c86865
frank.bejo <frankyenbejo@gmail.com>
2026-01-25 22:48:52 +0800

    added role guard for user authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
 import { PersonModule } from './person/person.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './enum/common/guards/roles.guard';
 
 @Module({
   imports: [AuthModule, PersonModule],
   // controllers: [AppController],
-  // providers: [],
+  providers: [APP_GUARD],
+  useClass: RolesGuard,
 })
 export class AppModule {}
